85133d891be767c9025858192ab424eee1531116
Aarthika <aarthikaa.devi@gmail.com>
2025-09-28 21:14:21 +0530

    files added

--- recommender.py
+++
@@ -1,9 +0,0 @@
-from database import internships
-
-def recommend_internships(user_history):
-    # Simple content-based filtering on skill overlap
-    recommendations = []
-    for internship in internships:
-        if set(internship["skills"]) & set(user_history):
-            recommendations.append(internship)
-    return recommendations
